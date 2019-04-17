cae9d45d1e6e247fa5a4b030872ba759be7aa203
Valery Kovshov <valershik39@gmail.com>
2019-04-17 23:37:06 +0300

    HW 1 with fix

--- src/main/java/ru/mail/polis/DAOFactory.java
+++ src/main/java/ru/mail/polis/DAOFactory.java
@@ -17,7 +17,7 @@
 package ru.mail.polis;
 
 import org.jetbrains.annotations.NotNull;
-import ru.mail.polis.prohladenn.DAOI;
+import ru.mail.polis.prohladenn.CustomDAO;
 
 import java.io.File;
 import java.io.IOException;
@@ -54,6 +54,6 @@ public final class DAOFactory {
             throw new IllegalArgumentException("Path is not a directory: " + data);
         }
 
-        return new DAOI();
+        return new CustomDAO();
     }
 }
